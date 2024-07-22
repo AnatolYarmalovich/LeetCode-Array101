8c37d03f483991fd8bd3790fd977b33609747c6f
AnatolDev <a.yarmolovich@verv.com>
2024-07-22 10:26:18 +0200

    Array101: Two Sum

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,5 +1,4 @@
-import UIKit
-import Darwin
+import Foundation
 
 /*
 
@@ -703,3 +702,98 @@ import Darwin
 //
 //let result = Solution().heightChecker([5,1,2,3,4])
 //print(result)
+
+/*
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ You can return the answer in any order.
+
+
+
+ Example 1:
+
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ Example 2:
+
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+ Example 3:
+
+ Input: nums = [3,3], target = 6
+ Output: [0,1]
+
+
+ Constraints:
+
+ 2 <= nums.length <= 104
+ -109 <= nums[i] <= 109
+ -109 <= target <= 109
+ Only one valid answer exists.
+
+
+ Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
+
+//class Solution {
+//
+//    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+//        var firstIndex = 0
+//        var secondIndex = 1
+//        while nums[firstIndex] + nums[secondIndex] != target || firstIndex == secondIndex {
+//            if secondIndex == nums.count - 1  {
+//                secondIndex = 0
+//                firstIndex += 1
+//            } else if firstIndex == nums.count - 1 {
+//                firstIndex = 0
+//            }
+//            else {
+//                secondIndex += 1
+//            }
+//        }
+//
+//        return [firstIndex, secondIndex]
+//    }
+//
+//    func firstTestCase() {
+//        let actual = twoSum([2,7,11,15], 9)
+//        let expected = [0, 1]
+//        let condition = actual == expected
+//        print("Actual: \(actual), Expected: \(expected), Test passed: \(condition)")
+//        assert(condition)
+//    }
+//
+//    func secondTestCase() {
+//        let actual = twoSum([3,2,4], 6)
+//        let expected = [1, 2]
+//        let condition = actual == expected
+//        print("Actual: \(actual), Expected: \(expected), Test passed: \(condition)")
+//        assert(condition)
+//    }
+//
+//    func thirdTestCase() {
+//        let actual = twoSum([3,3], 6)
+//        let expected = [0, 1]
+//        let condition = actual == expected
+//        print("Actual: \(actual), Expected: \(expected), Test passed: \(condition)")
+//        assert(condition)
+//    }
+//
+//    func fourthTestCase() {
+//        let actual = twoSum([2,5,5,11], 10)
+//        let expected = [1, 2]
+//        let condition = actual == expected
+//        print("Actual: \(actual), Expected: \(expected), Test passed: \(condition)")
+//        assert(condition)
+//    }
+//
+//}
+//
+//print(Solution().firstTestCase())
+//print(Solution().secondTestCase())
+//print(Solution().thirdTestCase())
+//print(Solution().fourthTestCase())
+
